a071012efdeed2cf9857d82ea9a134192a0b6f01
Mitchell Sternke <mitchellsternke@gmail.com>
2015-09-18 10:47:53 -0500

    Added more comments for private Level methods

--- source/level/Level.hpp
+++ source/level/Level.hpp
@@ -47,30 +47,129 @@ private:
 	std::list<Layer*> layers;
 	std::list<Sprite*> sprites;
 
+	/**
+	 * Check if a sprite is standing on top of any block in a given layer.
+	 */
 	bool isSpriteStandingOnLayer( const Layer* layer, const Sprite* sprite ) const;
+
+	/**
+	 * Move a layer down by one pixel.
+	 */
 	void moveLayerDown( Layer* layer );
+
+	/**
+	 * Move a layer to the left by one pixel.
+	 */
 	void moveLayerLeft( Layer* layer );
+
+	/**
+	 * Move a layer to the right by one pixel.
+	 */
 	void moveLayerRight( Layer* layer );
+
+	/**
+	 * Move a layer up by one pixel.
+	 */
 	void moveLayerUp( Layer* layer );
+
+	/**
+	 * Move a sprite down by one pixel.
+	 */
 	bool moveSpriteDown( Sprite* sprite );
+
+	/**
+	 * Move a sprite down by a given amount.
+	 */
 	void moveSpriteDown( Sprite* sprite, float dy );
+
+	/**
+	 * Move a sprite to the left by one pixel.
+	 */
 	bool moveSpriteLeft( Sprite* sprite );
+
+	/**
+	 * Move a sprite to the left by a given amount.
+	 */
 	void moveSpriteLeft( Sprite* sprite, float dx );
+
+	/**
+	 * Move a sprite to the right by one pixel.
+	 */
 	bool moveSpriteRight( Sprite* sprite );
+
+	/**
+	 * Move a sprite to the right by a given amount.
+	 */
 	void moveSpriteRight( Sprite* sprite, float dx );
+
+	/**
+	 * Move a sprite up by one pixel.
+	 */
 	bool moveSpriteUp( Sprite* sprite );
+
+	/**
+	 * Move a sprite up by a given amount.
+	 */
 	void moveSpriteUp( Sprite* sprite, float dy );
+
+	/**
+	 * See if it is possible to move a sprite down by one pixel.
+	 */
 	bool tryMoveSpriteDown( Sprite* sprite );
+
+	/**
+	 * See if it is possible to move a sprite to the left by one pixel.
+	 */
 	bool tryMoveSpriteLeft( Sprite* sprite );
+
+	/**
+	 * See if it is possible to move a sprite to the right by one pixel.
+	 */
 	bool tryMoveSpriteRight( Sprite* sprite );
+
+	/**
+	 * See if it is possible to move a sprite up by one pixel.
+	 */
 	bool tryMoveSpriteUp( Sprite* sprite );
+
+	/**
+	 * Update a layer by one frame.
+	 */
 	void updateLayer(Layer* layer);
+
+	/**
+	 * Update all layers by one frame.
+	 */
 	void updateLayers();
+
+	/**
+	 * Update layer motion in the x direction.
+	 */
 	void updateLayerXMotion( Layer* layer );
+
+	/**
+	 * Update layer motion in the y direction.
+	 */
 	void updateLayerYMotion( Layer* layer );
+
+	/**
+	 * Update a sprite by one frame.
+	 */
 	void updateSprite( Sprite* sprite );
+
+	/**
+	 * Update all sprites by one frame.
+	 */
 	void updateSprites();
+
+	/**
+	 * Update sprite motion in the x direction.
+	 */
 	void updateSpriteXMotion( Sprite* sprite );
+
+	/**
+	 * Update sprite motion in the y direction.
+	 */
 	void updateSpriteYMotion( Sprite* sprite );
 };
 
